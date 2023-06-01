502f4b3bc90557fd30a08ba20286514760d86c5c
elyes <elyeskacemeducation@gmail.com>
2023-06-01 16:17:32 +0100

    adding saving image into folder

--- troctrpretrained.py
+++ troctrpretrained.py
@@ -2,6 +2,9 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 import requests
 from io import BytesIO
+import time
+import os
+
 
 from fastapi import FastAPI, File, UploadFile
 
@@ -9,6 +12,12 @@ from fastapi import FastAPI, File, UploadFile
 # url = "Images/mhamed.png"
 # image = Image.open(url).convert("RGB")
 app = FastAPI()
+
+
+#Generating time function ms
+def current_milli_time():
+    return str(round(time.time() * 1000))
+
 
 # processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 # model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
@@ -31,6 +40,16 @@ async def process_image(image: UploadFile):
     generated_ids = model.generate(pixel_values)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
     print(generated_text)
+
+    #generating folder
+    path = "data/" + generated_text + "/"
+    filename = current_milli_time() + ".jpg"
+    full_path = os.path.join(path, filename)
+
+    # Create the directory if it doesn't exist
+    os.makedirs(path, exist_ok=True)
+    with open("data/"+generated_text+"/"+current_milli_time()+".jpg", "wb") as f:
+        f.write(image_io.getbuffer())
 
     # return the OCR result
     return {"ocr_result": generated_text}
